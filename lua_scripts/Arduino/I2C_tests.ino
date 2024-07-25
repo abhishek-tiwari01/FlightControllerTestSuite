9133c0aaf9495e5eb070707c97a38dd2a7e08b13
Abhishek Tiwari <72995040+abhishek-tiwari01@users.noreply.github.com>
2024-07-25 10:20:37 +0530

    Update I2C_tests.ino

--- lua_scripts/Arduino/I2C_tests.ino
+++ lua_scripts/Arduino/I2C_tests.ino
@@ -1,23 +1,14 @@
 #include <Arduino.h>
 #include <I2C_Slave.h>
 
-bool isI2CBus0 = true; // Flag to indicate the current I2C bus
-
 void setup() {
-    Slave.begin();
+    Slave.begin(); // Initialize I2C slave with default address 0x09
+    
+    // Write "1" to virtual registers to indicate the test data
+    char data[] = "1";
+    Slave.writeRegisters(data);
 }
 
 void loop() {
-    if (isI2CBus0) {
-        char I2C1[] = "I2C1 Tested!";
-        Slave.writeRegisters(I2C1);
-    } else {
-        char I2C2[] = "I2C2 Tested!";
-        Slave.writeRegisters(I2C2);
-    }
-    
-    // Toggle the I2C bus flag
-    isI2CBus0 = !isI2CBus0;
-
-    delay(2000); // Adjust delay as needed
+    // No functionality in loop
 }
